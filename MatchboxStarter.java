19b3b9d112d9b610c07f91f3f90eb333cf441519
geethabj <geetha.xworkz@gmail.com>
2023-10-31 12:22:20 +0530

    instance variable

---
+++ MatchboxStarter.java
@@ -0,0 +1,16 @@
+public class MatchboxStarter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Matchbox matchbox = new Matchbox("Geetha match works",5.5,25,2.50,'S',100.0,"wooden");
+		System.out.println("Matchbox brand is : "+matchbox.brand);
+		System.out.println("Matchbox length : "+matchbox.length);
+		System.out.println("Matchbox NoOfSticks are : "+matchbox.NoOfSticks);
+		System.out.println("Matchbox price is : "+matchbox.price);
+		System.out.println("Matchbox Size is : "+matchbox.Size);
+		System.out.println("Matchbox weight is : "+matchbox.weight);
+		System.out.println("Matchbox type is : "+matchbox.type);
+
+	}
+
+}
